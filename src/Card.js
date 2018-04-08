3d1272e012fbda3dd47111189251b25280a03918
luthermiller6042 <37706126+luthermiller6042@users.noreply.github.com>
2018-04-08 10:59:17 -0400

    hope this works

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ const Card = ({name, website, jobTitle, location, id})=> {
 	
 return(
 
-	<div className='tc bg-light-green dib w5  br3 pa3 ma3 grow bw2 shadow-5'>
+	<div className='tc bg-white dib w5  br3 pa3 ma3 grow bw2 shadow-5'>
 	 { id ?  id  : <img className="br-100 pa1 ba b--black h4 w4" alt='robots' src={`https://cdn3.iconfinder.com/data/icons/pictofoundry-pro-vector-set/512/Avatar-128.png` }/> }
 		
 
