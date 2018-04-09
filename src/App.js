fed33ed8c5fe85c93a22e9a859c0dbeddcc7655c
luthermiller6042 <37706126+luthermiller6042@users.noreply.github.com>
2018-04-09 09:15:40 -0400

    save the restored zip file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+	return ( 
+	<div className='tc'>
+		<h1>Job Board!</h1>
+		<SearchBox />
+		<CardList robots={ robots } />
+	</div>
+	);
 }
-
 export default App;
