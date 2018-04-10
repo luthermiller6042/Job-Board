54f3c39cb86c56539ef98a0bf98ebff39f6bd08b
luthermiller6042 <37706126+luthermiller6042@users.noreply.github.com>
2018-04-10 17:17:55 -0400

    remove react icon

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Scroll = (props) => {
-return (<div style={{overflowY:'scroll', borderTop: '2px solid black', height: '500px' }}>
+return (<div style={{overflowY:'scroll', borderTop: '2px solid black', height: '800px' }}>
 	{props.children}
 	</div>
 )
